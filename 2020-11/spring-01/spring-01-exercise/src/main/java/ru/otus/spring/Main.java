ea755b0b62c3cee41a56c706f98d4c790e5dd8a7
stsaplin <stsaplin@diasoft.ru>
2020-11-26 20:44:22 +0300

    пробую бины

--- 2020-11/spring-01/spring-01-exercise/src/main/java/ru/otus/spring/Main.java
+++ 2020-11/spring-01/spring-01-exercise/src/main/java/ru/otus/spring/Main.java
@@ -2,16 +2,18 @@ package ru.otus.spring;
 
 //import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.otus.spring.domain.Person;
+import ru.otus.spring.service.PersonService;
 
 public class Main {
 
     public static void main(String[] args) {
         // TODO: создайте здесь класс контекста
+        //ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
+        PersonService service = null; // context.getBean(PersonService.class);
 
         // TODO: Получите Person Service
-
         // Получите Person "Ivan"
-        Person ivan = null;
+        Person ivan = service.getByName("Ivan");
         System.out.println("name: " + ivan.getName() + " age: " + ivan.getAge());
     }
 }
