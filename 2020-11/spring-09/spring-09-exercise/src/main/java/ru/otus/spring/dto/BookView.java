a49a709d023ea48bcd8c3281fb52db6483eb8bd4
stsaplin <stsaplin@diasoft.ru>
2021-01-04 21:47:27 +0300

    getByBook

--- 2020-11/spring-09/spring-09-exercise/src/main/java/ru/otus/spring/dto/BookView.java
+++ 2020-11/spring-09/spring-09-exercise/src/main/java/ru/otus/spring/dto/BookView.java
@@ -1,7 +1,11 @@
 package ru.otus.spring.dto;
 
+import ru.otus.spring.domain.Book;
+import ru.otus.spring.domain.BookComment;
+
 import java.util.List;
 
 public interface BookView {
     List<BookDto> getAll();
+    List<BookComment> getByBook(Book book);
 }
--- 2020-11/spring-09/spring-09-exercise/src/main/java/ru/otus/spring/dto/BookViewImpl.java
+++ 2020-11/spring-09/spring-09-exercise/src/main/java/ru/otus/spring/dto/BookViewImpl.java
@@ -2,10 +2,13 @@ package ru.otus.spring.dto;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import ru.otus.spring.domain.Book;
+import ru.otus.spring.domain.BookComment;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
@@ -19,4 +22,13 @@ public class BookViewImpl implements BookView {
         Query query = em.createQuery("select new ru.otus.spring.dto.BookDto ( b.bookId, b.name, a.authorId, a.FIO, g.genreId, g.Name) from Book b join b.author a join b.genre g", BookDto.class);
         return query.getResultList();
     }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<BookComment> getByBook(Book book) {
+        Query query = em.createQuery("select bc from BookComment bc where bc.book = :book");
+        query.setParameter("book", book);
+        return query.getResultList();
+    }
+
 }
