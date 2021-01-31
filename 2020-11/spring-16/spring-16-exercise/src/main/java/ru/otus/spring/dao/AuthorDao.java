01274d3c10beb02c463b9ff98e84d4f89974774f
stsaplin <stsaplin@diasoft.ru>
2021-01-31 20:27:04 +0300

    Создание репо для задания по ThymeLeaf

---
+++ 2020-11/spring-16/spring-16-exercise/src/main/java/ru/otus/spring/dao/AuthorDao.java
@@ -0,0 +1,7 @@
+package ru.otus.spring.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.otus.spring.domain.Author;
+
+public interface AuthorDao extends JpaRepository<Author, Long> {
+}
---
+++ 2020-11/spring-16/spring-16-exercise/src/main/java/ru/otus/spring/service/LibraryImpl.java
@@ -0,0 +1,130 @@
+package ru.otus.spring.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+import org.springframework.transaction.annotation.Transactional;
+import ru.otus.spring.dao.AuthorDao;
+import ru.otus.spring.dao.BookCommentDao;
+import ru.otus.spring.dao.BookDao;
+import ru.otus.spring.dao.GenreDao;
+import ru.otus.spring.domain.Author;
+import ru.otus.spring.domain.Book;
+import ru.otus.spring.domain.BookComment;
+import ru.otus.spring.domain.Genre;
+
+import java.util.List;
+
+// Класс работы с приложением "Библиотека книг"
+@ShellComponent
+@RequiredArgsConstructor
+public class LibraryImpl implements Library {
+
+    private final BookDao bookDao;
+    private final BookCommentDao bookCommentDao;
+    private final AuthorDao authorDao;
+    private final GenreDao genreDao;
+    private final String BOOK_NOT_EXIST_MESSAGE = "Книга не существует в базе данных.";
+
+    private static Logger logger = LogManager.getLogger();
+
+    @ShellMethod(value = "Add a new book in format:ins authorId genreId bookName", key = {"ins", "insert"})
+    public boolean bookInsert(long authorId, long genreId, String bookName) throws Exception {
+        boolean result = false;
+
+        Author author = authorDao.findById(authorId).get();
+        Genre genre = genreDao.findById(genreId).get();
+        Book b = new Book(0, author, genre, bookName);
+        try {
+            bookDao.save(b);
+            result = true;
+            logger.info("Книга успешно добавлена.");
+        }catch (Exception e) {
+            logger.error(e.getMessage());
+            throw new Exception(e);
+        }
+
+        return result;
+    }
+
+    @ShellMethod(value = "Modify the selected book in format:upd bookId authorId genreId bookName", key = {"upd", "update"})
+    public boolean bookUpdate(long bookId, @ShellOption(defaultValue = "-1") long authorId, @ShellOption(defaultValue = "-1") long genreId, @ShellOption(defaultValue = ShellOption.NULL) String bookName) throws Exception {
+        boolean result = false;
+
+        Author author = authorDao.findById(authorId).get();
+        Genre genre = genreDao.findById(genreId).get();
+        Book b = new Book(bookId, author, genre, bookName);
+        try {
+            val optionalBook = bookDao.findById(bookId);
+            if (optionalBook.isPresent()) {
+                bookDao.save(b);
+                result = true;
+                logger.info("Книга успешно изменена.");
+            }
+            else {
+               logger.error(BOOK_NOT_EXIST_MESSAGE);
+            }
+        }catch (Exception e) {
+            logger.error(e.getMessage());
+            throw new Exception(e);
+        }
+
+        return result;
+    }
+
+    @ShellMethod(value = "Delete the selected book in format:del bookId", key = {"del", "delete"})
+    public boolean bookDelete(long bookId) throws Exception {
+        boolean result = false;
+        try {
+            val optionalBook = bookDao.findById(bookId);
+            if (optionalBook.isPresent()) {
+                bookCommentDao.deleteAllByBook(optionalBook.get());
+                bookDao.deleteById(bookId);
+                result = true;
+                logger.info("Книга успешно удалена.");
+            }
+            else {
+                logger.info(BOOK_NOT_EXIST_MESSAGE);
+            }
+        }catch (Exception e) {
+            logger.error(e.getMessage());
+            throw new Exception(e);
+        }
+
+        return result;
+    }
+
+    @ShellMethod(value = "Show all the books in the library", key = {"show"})
+    public void showAllBooks() throws Exception {
+        try {
+            List<Book> lb = bookDao.findBooksByAuthorNotNullAndGenreNotNull();
+            lb.forEach(logger::info);
+        }catch (Exception e) {
+            logger.error(e.getMessage());
+            throw new Exception(e);
+        }
+    }
+
+    @ShellMethod(value = "Show comments of selected book in format:comments bookId", key = {"comments"})
+    @Transactional
+    public void showCoomentsByBook(long bookId) throws Exception {
+        try {
+            val optionalBook = bookDao.findById(bookId);
+            if (optionalBook.isPresent()) {
+                List<BookComment> lbc = bookCommentDao.findAllByBook(optionalBook.get());
+                System.out.println("Комментарии книги bookId=" + String.valueOf(bookId));
+                lbc.forEach(System.out::println);
+            } else {
+                logger.info(BOOK_NOT_EXIST_MESSAGE);
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            throw new Exception(e);
+        }
+    }
+
+}
