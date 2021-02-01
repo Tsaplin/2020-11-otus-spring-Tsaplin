84901829e01c3464f21a1de5bd086a34d5f671b7
stsaplin <stsaplin@diasoft.ru>
2021-02-01 22:53:41 +0300

    Страницы редактирования книги и ее удаления

--- 2020-11/spring-16/spring-16-exercise/src/main/java/ru/otus/spring/controller/BookController.java
+++ 2020-11/spring-16/spring-16-exercise/src/main/java/ru/otus/spring/controller/BookController.java
@@ -1,27 +1,31 @@
 package ru.otus.spring.controller;
 
 import javassist.NotFoundException;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.otus.spring.dao.BookDao;
 import ru.otus.spring.domain.Book;
+import ru.otus.spring.service.Library;
+import ru.otus.spring.service.LibraryImpl;
 
 import java.util.Optional;
 
-
 @Controller
+@AllArgsConstructor
+@Import({LibraryImpl.class})
 public class BookController {
     private final BookDao bookDao;
-
-    @Autowired
-    public BookController(BookDao bookDao) {
-        this.bookDao = bookDao;
-    }
+    private final Library library;
 
     @GetMapping("/editBook")
     public String bookFind(@RequestParam("bookId") long bookId, Model model) {
@@ -38,9 +42,29 @@ public class BookController {
             Model model
     ) {
         Book saved = bookDao.save(book);
+       // library.bookUpdate()
         model.addAttribute("book", saved);
         return "editBook";
     }
 
+    @GetMapping("/deleteBook")
+    public String bookFindForDelete(@RequestParam("bookId") long bookId, Model model) {
+        bookFind(bookId, model);
+        return "deleteBook";
+    }
+
+    @PostMapping("/deleteBook")
+    public String bookDelete(
+            Book book, Model model
+    ) {
+        try {
+            library.bookDelete(book.getBookId());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        model.addAttribute("book", new Book());
+        return "deleteBook";
+    }
 
 }
--- 2020-11/spring-16/spring-16-exercise/src/main/resources/data.sql
+++ 2020-11/spring-16/spring-16-exercise/src/main/resources/data.sql
@@ -7,5 +7,6 @@ insert into tGenre (GenreID, `Name`) values (2, 'Экономика');
 insert into tGenre (GenreID, `Name`) values (3, 'Фантастика');
 
 insert into tBook (BookID, AuthorID, GenreID, `Name`) values (1, 2, 2, 'Тестовая книга для комментариев');
+insert into tBook (BookID, AuthorID, GenreID, `Name`) values (2, 3, 3, 'Вторая книга для комментариев');
 insert into tBookComment (BookCommentID, BookID, Comment) values (101, 1, 'Первый комментарий для Тестовая книга');
 insert into tBookComment (BookCommentID, BookID, Comment) values (102, 1, 'Второй комментарий для Тестовая книга');
