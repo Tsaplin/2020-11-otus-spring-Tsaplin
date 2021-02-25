60db6a152004ea85ce1f0bc54a99d1506135ac84
stsaplin <stsaplin@diasoft.ru>
2021-02-25 09:49:25 +0300

    Разбираемся с монгой

--- 2020-11/spring-13/spring-13-exercise/src/main/java/ru/otus/spring/Main.java
+++ 2020-11/spring-13/spring-13-exercise/src/main/java/ru/otus/spring/Main.java
@@ -1,6 +1,5 @@
 package ru.otus.spring;
 
-import org.h2.tools.Console;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -8,9 +7,6 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 import ru.otus.spring.dao.AuthorDao;
 import ru.otus.spring.dao.BookDao;
 import ru.otus.spring.dao.GenreDao;
-import ru.otus.spring.domain.Author;
-import ru.otus.spring.domain.Book;
-import ru.otus.spring.domain.Genre;
 import ru.otus.spring.mongock.changelog.DatabaseChangelog;
 import ru.otus.spring.service.Library;
 
