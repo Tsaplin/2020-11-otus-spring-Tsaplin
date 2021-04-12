34fcf22c0065afe7d860e9bb1c19c3af6bc303a0
stsaplin <stsaplin@diasoft.ru>
2021-04-12 21:36:21 +0300

    Убрать MediaType

--- 2020-11/spring-20-MongoReactive/spring-20-exercise/src/main/java/ru/otus/spring/controller/BookAsyncController.java
+++ 2020-11/spring-20-MongoReactive/spring-20-exercise/src/main/java/ru/otus/spring/controller/BookAsyncController.java
@@ -5,6 +5,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import ru.otus.spring.dao.BookDao;
 import ru.otus.spring.domain.Book;
 import ru.otus.spring.service.Library;
 
@@ -13,7 +14,7 @@ import ru.otus.spring.service.Library;
 public class BookAsyncController {
     private final Library library;
 
-    @GetMapping(path = "/all-books", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @GetMapping(path = "/all-books")
     public Flux<Book> getAllLibraryBooks() {
         return library.showAllBooks();
     }
