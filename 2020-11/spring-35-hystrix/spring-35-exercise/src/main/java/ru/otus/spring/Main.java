84cc3b80c704ee4c02a43f7485764646d22eea9c
stsaplin <stsaplin@diasoft.ru>
2021-04-18 19:52:56 +0300

    Подключил HYSTRIX DASHBOARD

--- 2020-11/spring-35-hystrix/spring-35-exercise/src/main/java/ru/otus/spring/Main.java
+++ 2020-11/spring-35-hystrix/spring-35-exercise/src/main/java/ru/otus/spring/Main.java
@@ -3,9 +3,12 @@ package ru.otus.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+//import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @EnableCircuitBreaker
-//@EnableHystrixDashboard
+//@EnableTurbine
+@EnableHystrixDashboard
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
